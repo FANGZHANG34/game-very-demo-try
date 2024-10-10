7f8ef0805b84db91389c10ead3b7e213d25a9428
方丈34 <168342791+FANGZHANG34@users.noreply.github.com>
2024-10-10 08:31:13 +0800

    Add files via upload

--- countRows.js
+++ countRows.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
-['.\\js\\main0.js','.\\js\\main1.js','.\\js\\fn.js','.\\js\\classPeople.js','.\\js\\classPeople1.js','.\\js\\extension\\undertale-like-system.js'].
-reduce((s,path)=>fs.readFile(path,'utf-8',(e,value)=>{
-    const testReg = /^[\s\t]{0,}[/\*]/;
+[
+    'main0.js',
+    'main1.js',
+    'fn.js',
+    'init.js',
+    'classPeople1.js',
+    'extension\\undertale-like-system.js',
+].map(str=>'.\\gameTry\\js\\'+str).reduce((s,path)=>fs.readFile(path,'utf-8',(e,value)=>{
+    const testReg = /^[\s\t]{0,}[/*]/,testReg1 = /^[\s\t]{0,}[)}];?$/;
     console.log(
-        path,e ?? value.split(/\n|\r/).
-        reduce((c,str)=>(str && (testReg.test(str) ? c[1]++ : c[0]++),c),[0,0]).
+        path,e ?? value.split(/[\n\r\t]/).
+        reduce((c,str)=>(str && ((testReg.test(str) || testReg1.test(str)) ? c[1]++ : c[0]++),c),[0,0]).
         reduce((s,v,i,arr)=>i === 1 ? [...arr,arr[0] / (arr[0] + arr[1])] : s)
     );
 }),0);
